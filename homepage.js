c1d5d6afbe401ecf845298ddad2f40f6247db625
Osamah2004 <165146752+Osamah2004@users.noreply.github.com>
2024-05-12 22:09:48 +0300

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,18 @@
+// انتظر حتى يتم تحميل المستند بالكامل
+document.addEventListener("DOMContentLoaded", function() {
+    // احضر قائمة جميع الصور في الصفحة
+    const images = document.querySelectorAll('.product img');
+
+    //    تطبيق التأثير عندما يتم تحويل المؤشر فوقها وعندما يتوقف تحويل المؤشر عنها
+    images.forEach(image => {
+        image.addEventListener('mouseenter', () => {
+            image.style.transform = 'scale(1.1)';
+            image.style.transition = 'transform 0.3s ease';
+        });
+
+        image.addEventListener('mouseleave', () => {
+            image.style.transform = 'scale(1)';
+            image.style.transition = 'transform 0.3s ease';
+        });
+    });
+});
